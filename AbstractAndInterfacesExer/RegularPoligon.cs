169d0115234023680e70ce09e7270f56913c68b8
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-01-19 23:51:41 +0100

    Add abstract class with abstract method.

---
+++ AbstractAndInterfacesExer/RegularPoligon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractAndInterfacesExer
+{
+    public abstract class RegularPoligon
+    {
+        public RegularPoligon(int sideLenght , int sideNumber)
+        {
+            SideLenght = sideLenght;
+            SideNumber = sideNumber;
+
+        }
+        public int SideLenght { get; set; }
+        public int SideNumber { get; set; }
+
+        public int CalculatePerimeter()
+        {
+            return SideLenght * SideNumber;
+        }
+
+        public abstract double CalculateArea();
+        
+
+    }
+}
