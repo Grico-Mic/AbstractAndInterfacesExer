78dbda9fca6f57246927cfddd3006875b90fd2a7
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-01-19 23:57:53 +0100

    Add abstract method "CalculateArea()"

---
+++ AbstractAndInterfacesExer/RegularTriangle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractAndInterfacesExer
+{
+    public class RegularTriangle : RegularPoligon
+    {
+        public RegularTriangle(int sideLenght) : base(sideLenght,3)
+        {
+
+        }
+        public override double CalculateArea()
+        {
+            return SideLenght * SideLenght * Math.Sqrt(3) / 4;
+        }
+    }
+}
---
+++ AbstractAndInterfacesExer/Square.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractAndInterfacesExer
+{
+    public class Square : RegularPoligon
+    {
+        public Square(int sideLenght) : base(sideLenght, 4)
+        {
+
+        }
+
+        public override double CalculateArea()
+        {
+            return  SideLenght * SideLenght;
+        }
+    }
+}
