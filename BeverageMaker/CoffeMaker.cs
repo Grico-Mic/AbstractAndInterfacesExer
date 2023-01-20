43e50194983619c3cab6138971a29c85fb5948ff
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-01-20 23:51:26 +0100

    Add new project with classes

---
+++ BeverageMaker/CoffeMaker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeverageMaker
+{
+   public  class CoffeMaker
+    {
+        public void AddWater()
+        {
+            Console.WriteLine("Add water");
+        }
+
+        public void AddOnStove()
+        {
+            Console.WriteLine("Add on stove");
+        }
+
+        public void BoilingWater()
+        {
+            Console.WriteLine("Tne water is boiled");
+        }
+
+        public void AddCoffe()
+        {
+            Console.WriteLine("Add coffe");
+        }
+
+        public void ServedBeverage()
+        {
+            Console.WriteLine("The beverage is served");
+        }
+    }
+} 
---
+++ BeverageMaker/TeaMaker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeverageMaker
+
+{
+    public class TeaMaker
+    {
+        public void AddWater()
+        {
+            Console.WriteLine("Add water");
+        }
+
+        public void AddOnStove()
+        {
+            Console.WriteLine("Add on stove");
+        }
+
+        public void BoilingWater()
+        {
+            Console.WriteLine("Tne water is boiled");
+        }
+
+        public void AddTea()
+        {
+            Console.WriteLine("Add tea");
+        }
+        public void ServedBeverage()
+        {
+            Console.WriteLine("The beverage is served");
+        }
+
+    }
+}
