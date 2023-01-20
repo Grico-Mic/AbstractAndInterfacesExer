d1f805e330997dc83faafd374a216dc6fda64ba3
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-01-21 00:18:05 +0100

    Add abstract class with abstract method

---
+++ BeverageMaker/BeverageMaker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace BeverageMaker
+{
+    public abstract class BeverageMaker
+    {
+        public void ServedBeverage()
+        {
+            AddWater();
+            AddOnStove();
+            BoilingWater();
+            AddBeverage();
+
+            Console.WriteLine("The beverage is served");
+        }
+        private void AddWater()
+        {
+            Console.WriteLine("Add water");
+        }
+        private void AddOnStove()
+        {
+            Console.WriteLine("Add on stove");
+        }
+
+        private void BoilingWater()
+        {
+            Console.WriteLine("Tne water is boiled");
+        }
+
+        public abstract void AddBeverage();
+        
+       
+    }
+}
