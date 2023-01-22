2666dddcac3a8affec1c747af649918c83787f9d
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-01-22 23:44:16 +0100

    Add Factory Pattern with logic

---
+++ BeverageMaker/BeverageMakerFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeverageMaker
+{
+    public class BeverageMakerFactory
+    {
+        public IBeverageMaker CreateBeverageMaker(string userInput)
+        {
+            IBeverageMaker maker = null;
+            if (userInput == "1")
+            {
+                maker = new TeaMaker();
+
+            }
+            else if (userInput == "2")
+            {
+                maker = new CoffeMaker();
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+           return maker;
+        }
+    }
+}
